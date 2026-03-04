439907471fbf0bb60c441d6599370f1b4f9250e8
optimistic <fedorova.alina0206@gmail.com>
2026-03-04 22:33:09 +0900

    lab5

--- app.py
+++ app.py
@@ -1,16 +1,21 @@
 import json
-from flask import Flask, abort, jsonify, request
-from flask_restx import Api  # type: ignore
+import os
+from flask import Flask, abort, request, jsonify
+from flask_restx import Api, Resource # type: ignore
 
 PAGE_SIZE = 25
 
 app = Flask(__name__)
 api = Api(app)
 
-with open('awards.json', encoding='utf-8') as f:
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+with open(os.path.join(BASE_DIR, 'awards.json'), encoding='utf-8') as f:
     awards = json.load(f)
 
-# TODO: Добавить код для чтения лауреатов из файла
+with open(os.path.join(BASE_DIR, 'laureats.json'), encoding='utf-8') as f:
+    laureats = json.load(f)
 
 
 @app.route("/api/v1/awards/")
@@ -20,11 +25,13 @@ def awards_list():
         if p < 0:
             raise ValueError
     except ValueError:
-        return abort(400)
-    page = awards[p * 50:(p + 1)*50]
+        abort(400)
+
+    page = awards[p * PAGE_SIZE:(p + 1) * PAGE_SIZE]
+
     return jsonify({
         'page': p,
-        'count_on_page': PAGE_SIZE,
+        'count_on_page': len(page),
         'total': len(awards),
         'items': page,
     })
@@ -38,7 +45,23 @@ def award_object(pk):
         abort(404)
 
 
-# TODO: Добавить код для получения списка лауреатов
+@api.route("/v2/laureats/")
+class LaureatsList(Resource):
+    def get(self):
+        return {
+            "total": len(laureats),
+            "items": laureats
+        }, 200
 
 
-# TODO: Добавить код для получения лауреата по индексу
+@api.route("/v2/laureat/<int:pk>/")
+class LaureatObject(Resource):
+    def get(self, pk):
+        if 0 <= pk < len(laureats):
+            return laureats[pk], 200
+        else:
+            abort(404)
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8000)
